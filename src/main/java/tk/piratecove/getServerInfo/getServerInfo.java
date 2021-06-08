28139320fe91629aeac373c21f8e198fdfb975b4
Jarne Theys <jarne.theys@student.ucll.be>
2021-06-08 21:53:45 +0200

    Change file used

--- src/main/java/tk/piratecove/getServerInfo/getServerInfo.java
+++ src/main/java/tk/piratecove/getServerInfo/getServerInfo.java
@@ -1,40 +1,12 @@
 package tk.piratecove.getServerInfo;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 public class getServerInfo extends JavaPlugin {
-    private Collection getPlayers(){
-        Collection<? extends Player> players = Bukkit.getOnlinePlayers();
-        return players;
-    }
-
     @Override
     public void onEnable() {
         getLogger().info("getServerInfo has been enabled");
         this.getCommand("writePLayers").setExecutor(new writePlayers());
-
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                getPlayers();
-                getLogger().info("getServerInfo has collected all the players");
-            }
-        },600000,600000);
-
     }
 
     @Override
--- src/main/java/tk/piratecove/getServerInfo/writePlayers.java
+++ src/main/java/tk/piratecove/getServerInfo/writePlayers.java
@@ -17,12 +17,9 @@ public class writePlayers implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         Logger log = Bukkit.getLogger();
-        if(!commandSender.getName().equalsIgnoreCase("OGCaptainCapsize") && commandSender instanceof Player){
-            log.info(commandSender.getName() + " Tried to use the writePlayers command");
-        }
         Collection<? extends Player> players = Bukkit.getOnlinePlayers();
         try {
-            File file = new File("players.txt");
+            File file = new File("C:\\MCServerFiles\\players.txt");
             if (file.createNewFile()) {
                 log.info("A new players file has been created");
             } else {
@@ -32,17 +29,19 @@ public class writePlayers implements CommandExecutor {
             log.info("An IOException has occured during the creation of the players file");
         }
         try {
-            FileWriter fileWriter = new FileWriter("players.txt");
+            String result = "";
+            FileWriter fileWriter = new FileWriter("C:\\MCServerFiles\\players.txt");
             for (Object player : players) {
                 if (player instanceof Player) {
                     Player player1 = (Player) player;
                     String playername = player1.getName();
                     String gamemode = player1.getGameMode().toString();
-                    fileWriter.write("Playername: " + playername + " Gamemode: " + gamemode);
-                    fileWriter.flush();
-                    fileWriter.close();
+                    result += ("Playername: " + playername + " Gamemode: " + gamemode + "\n");
                 }
             }
+            fileWriter.write(result);
+            fileWriter.flush();
+            fileWriter.close();
         } catch (IOException exception) {
             log.info("An IOException has occured during the writing of the players file");
 
